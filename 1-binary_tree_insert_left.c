2e1834196bb30c5b9a90aa857c1ea4ba6ac38465
SFerFer1 <10048@holbertonstudents.com>
2024-12-10 13:42:38 +0000

    ej 1 ej 2

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -9,12 +9,11 @@
 */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-if (parent == NULL)
-return (NULL);
-
-
 binary_tree_t *new_tree = malloc(sizeof(binary_tree_t));
 if (new_tree == NULL)
+return (NULL);
+
+if (parent == NULL)
 return (NULL);
 
 new_tree->n = value;
