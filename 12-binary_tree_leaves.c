4dceff8343a8b6c31e8aeb82b8f94b592ba6e4b0
SFerFer1 <10048@holbertonstudents.com>
2024-12-10 20:04:07 +0000

    task 12 error in 54

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -12,13 +12,14 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 	size_t count = 0;
 
 	if (tree == NULL)
-		return (0);
-
-	binary_tree_leaves(tree->left);
-	binary_tree_leaves(tree->right);
-
+	return (0);
+	
 	if (tree->left == NULL && tree->right == NULL)
-		count++;
+        return(1);
+
+count += binary_tree_leaves(tree->left);
+count += binary_tree_leaves(tree->right);
+
 
 	return (count);
 }
