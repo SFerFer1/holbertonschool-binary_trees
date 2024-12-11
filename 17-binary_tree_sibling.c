caa7f6afc2035939bef06b620fad46bfd3d53f74
SFerFer1 <10048@holbertonstudents.co>
2024-12-11 19:30:35 +0000

    task 17

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -8,11 +8,24 @@
 */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
+	if (node->parent->left == node)
+	{
+		if (node->parent->right == NULL)
+			return (NULL);
+		else
+			return (node->parent->right);
+
+	}
 
 
-
-
-
-
-
+	if (node->parent->right == node)
+	{
+		if (node->parent->left == NULL)
+			return (NULL);
+		else
+			return (node->parent->left);
+	}
+	return (NULL);
 }
