2903afbf2e91ea5a41b3a42a361fde59ca598ee2
SFerFer1 <10048@holbertonstudents.co>
2024-12-11 19:09:39 +0000

    ddddddddddd

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+/**
+* *binary_tree_sibling - checks if a node is a leaf.
+*
+*@node: a pointer to a parent node of the node to create.
+*
+*Return:  1 if node is a leaf, otherwise 0.
+*/
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+
+
+
+
+
+
+
+}
