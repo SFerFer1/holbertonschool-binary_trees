cd53308cace354c26ec153dcb4d62ba47f5cc434
SFerFer1 <10048@holbertonstudents.co>
2024-12-11 17:54:58 +0000

    dddddddddddddddddddddd

---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,121 @@
+#include "binary_trees.h"
+#include <stdio.h>
+/**
+*binary_tree_is_leaf - checks if a node is a leaf.
+*
+*@node: a pointer to a parent node of the node to create.
+*
+*Return:  1 if node is a leaf, otherwise 0.
+*/
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node == NULL)
+		return (0);
+
+	if (node->left == NULL && node->right == NULL)
+		return (1);
+
+	return (0);
+}
+/**
+ * binary_tree_is_full - checks if a binary tree is full.
+ * @tree: The root.
+ *
+ * Return: The number of the leaves or 0
+ */
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	int left, right;
+
+	if (tree == NULL)
+		return (0);
+
+	if (binary_tree_is_leaf(tree))
+		return (1);
+
+	if (tree->parent == NULL && tree->left == NULL && tree->right == NULL)
+		return (1);
+
+	left = binary_tree_is_full(tree->left);
+	right = binary_tree_is_full(tree->right);
+
+
+	if (left && right)
+		return (1);
+	else
+		return (0);
+}
+/**
+*binary_tree_height - goes through a binary tree using in-order traversal.
+*
+*@tree:  is a pointer to the root node of the tree to traverse.
+*
+*Return: return heigth of the tree or 0.
+*/
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t h_left, h_right;
+
+	if ((tree == NULL) || (tree->left == NULL && tree->right == NULL))
+		return (0);
+
+	h_left = binary_tree_height(tree->left);
+	h_right = binary_tree_height(tree->right);
+
+	if (h_left > h_right)
+		return (1 + h_left);
+	else
+		return (1 + h_right);
+
+}
+
+#include "binary_trees.h"
+
+/**
+ * binary_tree_depth - Serch depth of the tree
+ * @tree: Is a node of the bainary tree
+ *
+ * Return: The depth
+ */
+
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	size_t count = 0;
+
+	if ((tree == NULL) || (tree->parent == NULL))
+		return (count);
+
+	count = binary_tree_depth(tree->parent);
+	return (1 + count);
+}
+
+/**
+*binary_tree_is_perfect - checks if a node is a root.
+*
+*@tree: a pointer to a parent node of the node to create.
+*
+*Return:  1 if node is a root, otherwise 0.
+*/
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	size_t height = binary_tree_height(tree);
+
+	if (tree == NULL)
+		return (0);
+	if (!binary_tree_is_full(tree))
+		return (0);
+	else
+	{
+		printf("2");
+		if (tree->left == NULL && tree->right == NULL)
+		{
+		binary_tree_is_perfect(tree);
+		}
+		binary_tree_depth(tree);
+
+		if (binary_tree_depth(tree) == height)
+			return (1);
+		else
+			return (0);
+	}
+}
